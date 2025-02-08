0c61c0b6d8d397ac82c115df6b8157a4ad812a5d
xuexijiaoben <85658957+xuexijiaoben@users.noreply.github.com>
2025-02-08 14:05:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -37,19 +37,12 @@ function merge_package() {
 }
 
 merge_package main https://github.com/kenzok8/small-package package/app luci-app-adguardhome
-# merge_package master https://github.com/coolsnowwolf/luci package/app applications/luci-app-unblockmusic
 merge_package v5 https://github.com/sbwml/luci-app-mosdns package/app luci-app-mosdns mosdns v2dat
 merge_package main https://github.com/kenzok8/small-package package/app luci-app-fileassistant
 
-echo 'src-git alist https://github.com/sbwml/luci-app-alist.git' >>feeds.conf.default
-echo 'src-git amlogic https://github.com/ophub/luci-app-amlogic.git' >>feeds.conf.default
 echo 'src-git lucky https://github.com/gdy666/luci-app-lucky.git' >>feeds.conf.default
 git clone -b js --depth 1 https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/app/luci-app-unblockneteasemusic
-
-# Add a feed source
-# 添加额外软件包
-
-# 科学上网插件
+git clone -b master https://github.com/sbwml/luci-app-qbittorrent package/app/qbittorrent
 
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> "feeds.conf.default"
@@ -57,8 +50,7 @@ echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >
 
 # 开发版openclash
 merge_package dev https://github.com/vernesong/OpenClash package/app luci-app-openclash
-# 下载openclash内核
-#如果 core 文件夹不存在，创建文件夹
+# 下载openclash内核,如果 core 文件夹不存在，创建文件夹
 if [ ! -d "./files/etc/openclash/core" ]; then
   mkdir -p files/etc/openclash/core
 fi
@@ -86,29 +78,9 @@ sed -i "s/neon-vfpv4/vfpv4/" target/linux/amlogic/meson8b/target.mk
 rm -rf package/feeds/routing/batman-adv
 
 ./scripts/feeds update -a
-\rm -rf feeds/packages/net/cdnspeedtest
-\rm -rf feeds/ssrplus/mosdns
-\rm -rf feeds/packages/net/mosdns
-# \rm -rf feeds/luci/applications/luci-app-mosdns feeds/packages/utils/v2dat
-\rm -rf feeds/packages/net/alist
-\rm -rf feeds/luci/applications/luci-app-passwall
-\rm -rf feeds/luci/applications/luci-app-alist
-\rm -rf feeds/luci/applications/luci-app-unblockneteasemusic
-
-rm -rf feeds/luci/applications/luci-app-openclash
-# fix shadowsocks-rust
-rm feeds/packages/net/shadowsocks-rust/Makefile
-cp feeds/passwall_packages/shadowsocks-rust/Makefile feeds/packages/net/shadowsocks-rust
-# wget -P feeds/packages/net/shadowsocks-rust https://raw.githubusercontent.com/kenzok8/small-package/main/shadowsocks-rust/Makefile
 
 merge_package openwrt-23.05 https://github.com/coolsnowwolf/luci feeds/luci/applications applications/luci-app-pppwn
 merge_package master https://github.com/coolsnowwolf/packages feeds/packages/multimedia multimedia/pppwn-cpp
-
-# fix : ubus_call_umdns()=ubus.c@1385
-rm package/network/services/umdns/files/umdns.init
-rm package/network/services/umdns/files/umdns.json
-wget -P package/network/services/umdns/files https://raw.githubusercontent.com/openwrt/openwrt/main/package/network/services/umdns/files/umdns.init
-wget -P package/network/services/umdns/files https://raw.githubusercontent.com/openwrt/openwrt/main/package/network/services/umdns/files/umdns.json
 
 # AdguardHome core
 mkdir -p files/usr/bin
@@ -118,8 +90,6 @@ chmod +x files/usr/bin/AdGuardHome
 
 ./scripts/feeds install -a
 
-# 修复feeds错误
-sed -i 's/+libpcre/+libpcre2/g' package/feeds/telephony/freeswitch/Makefile
 
 # 主题
 
