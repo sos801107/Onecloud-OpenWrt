04364702ac6ae08e7a09df2c4e72cf870251c949
xuexijiaoben <85658957+xuexijiaoben@users.noreply.github.com>
2025-02-20 13:11:39 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -28,16 +28,15 @@ echo 'net.netfilter.nf_conntrack_max=65536' >>package/kernel/linux/files/sysctl-
 # 添加无线网卡支持
 sed -i '/bool "Enable SDIO bus interface support"/a\		default y if TARGET_amlogic' package/kernel/mac80211/broadcom.mk
 
-# 替换终端为bash
-# sed -i 's/\/bin\/ash/\/bin\/bash/' package/base-files/files/etc/passwd
+# AdguardHome core
+# mkdir -p files/usr/bin
+# AGH_CORE=$(curl -sL https://api.github.com/repos/AdguardTeam/AdGuardHome/releases/latest | grep /AdGuardHome_linux_armv7 | awk -F '"' '{print $4}')
+# wget -qO- $AGH_CORE | tar xOvz > files/usr/bin/AdGuardHome
+# chmod +x files/usr/bin/AdGuardHome
 
-# 删除默认密码
-# sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
-
-# 禁用wifi
-sed -i 's/${defaults ? 0 : 1}/1/g' package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
-# 修改wifi名字
-sed -i 's/OpenWrt/OneCloud/g' package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
+# resize
+chmod +x files/root/expand-root.sh
+chmod +x files/root/resize.sh
 
 #修改默认时区
 sed -i "s/timezone='UTC'/timezone='CST-8'/g" package/base-files/files/bin/config_generate
